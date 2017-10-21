9287b73bb053ac2e957043db13556c58f5ef476f
yas15 <31019957+yas15@users.noreply.github.com>
2017-10-21 21:15:48 +0100

    Add project files.

---
+++ App4/App4/IntValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    // Forms.Behavior<T> Class: Base generic class for generalized user-defined 
+    // behaviors that can respond to arbitrary conditions and events.
+    // Since the class IntValidationBehavior inherits from the class Behavior<T>
+    // we can now start to use Behavior<T> class methods in the class IntValidationBehavior
+    class IntValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool isValid = int.TryParse(e.NewTextValue, out int result);
+            Entry entry = sender as Entry;
+            entry.TextColor = isValid ? Color.Default : Color.Red;
+        }
+
+    }
+}
